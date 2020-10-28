18d13718ca896e954b58aeeb47b6ae71fc024d9a
sahilabs <73187203+sahilabs@users.noreply.github.com>
2020-10-28 23:25:44 +0530

    Update Sampler.py

--- Sampler.py
+++ Sampler.py
@@ -6,13 +6,13 @@ def Sampler(slices,pixel_array,n_size=50):
               nxt=pixel_array[i+1]
               tmp=np.abs(np.subtract(curr,nxt))
               curr=np.abs(curr)+1
-              tmp=np.divide(tmp,curr)
+              tmp=np.divide(tmp,curr)#DeltaX/X
               tmp=np.sum(tmp)
               change.append([i,tmp])
           change=np.array(change)
-          change[:,1]=change[:,1]/(nxt.shape[0]*nxt.shape[1])
+          change[:,1]=change[:,1]/(nxt.shape[0]*nxt.shape[1])#Normalize
           pixel_array=np.array(pixel_array)
-          ind_=sorted(np.argsort(np.gradient(change[:,1]*100))[::-1][0:50])#
+          ind_=sorted(np.argsort(np.gradient(change[:,1]*100))[::-1][0:n_size])#select best n_size
           pixel_array=pixel_array[ind_]
           slices=slices[ind_]
           del ind_,change,curr,nxt,tmp
